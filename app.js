c13ea75035357e88d5384a3f80b4df8ab6484b87
zj <515280674@qq.com>
2019-06-10 10:35:32 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import router from './routes/index.js'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import connectMongo from 'connect-mongo'
+import history from 'connect-history-api-fallback'
+import chalk from 'chalk'
+
+const config = require('config-lite')(__dirname);
+
+const app=express();
+
+app.all('*',(req,res,next)=>{
+    const {origin,Origin,referer,Referer} = req.headers;
+    const allowOrigin=origin || Origin || referer || Referer || '*';
+    res.header('Access-Control-Allow-Origin',allowOrigin);
+    res.header('Access-Control-Allow-Headers',"Content-Type,Authorization,X-Requested-Width");
+    res.header('Access-Control-Allow-Methods',"PUT,POST,GET,DELETE,OPTIONS");
+    res.header('Access-Control-Allow-Credentials',true);
+    res.header('X-Powered-By','Express');
+    if(req.method=='OPTIONS'){
+        res.sendStatus(200);
+    }else{
+        next();
+    }
+});
+
+app.use(cookieParser());
+
+const MongoStore=connectMongo(session);
+
+app.use(session({
+    name:config.session.name,
+    secret:config.session.secret,
+    resave:true,
+    saveUninitialized:false,
+    cookie:config.session.cookie,
+    store:new MongoStore({
+        url:config.url
+    })
+}));
+
+router(app);
+
+app.use(history());
+// app.use(express.static('./public'));
+
+app.listen(config.port,()=>{
+    console.log(
+        chalk.green(`成功监听端口:${config.port}`)
+    );
+});
