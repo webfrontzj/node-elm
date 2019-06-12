181f14500303752b4c8e082a04f1f12ecd440984
zhangjing <jingjing13140203@163.com>
2019-06-12 23:26:52 +0800

    at hotme

--- controller/v1/cities.js
+++ controller/v1/cities.js
@@ -2,3 +2,44 @@
 
 import Cities from '../../models/v1/cities'
 import pinyin from 'pinyin'
+import AddressComponent from '../../prototype/addressComponent'
+
+class CityHandle extends AddressComponent{
+    constructor(){
+        super();
+        this.getCity=this.getCity.bind(this);
+        this.getExactAddress=this.getExactAddress.bind(this);
+        this.pois=this.pois.bind(this);
+    }
+    async getCity(req,res,next){
+        const type=req.query.type;
+        let cityInfo;
+        try{
+            switch(type){
+                case 'guess':
+                    const city=await this.getCityName(req);
+                    cityInfo=await Cities.cityGuess(city);
+                    break;
+                case 'hot':
+                    cityInfo=await Cities.CityHot();
+                    break;
+                case 'group':
+                    cityInfo=await Cities.cityGroup();
+                    break;
+                default:
+                    res.json({
+                        name:'ERROR_QUERY_TYPE',
+                        message:'参数错误'
+                    });
+                    return;
+            }
+            res.send(cityInfo);
+        }catch(err){
+            res.send({
+                name:'ERROR_DATA',
+                message:'获取数据失败'
+            });
+        }
+    }
+    
+}
